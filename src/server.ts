e253956dd9e07b0418b160c1e7e6e92540aa8ee0
tarekul42 <tarekulrifat205@gmail.com>
2025-06-20 02:45:40 +0600

    basic server setup done and connected to mongodb via mongoose

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import config from "./config";
+import mongoose from "mongoose";
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to Library Management Server!",
+  });
+});
+
+async function server() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("Database connected successfully");
+
+    app.listen(config.port, () => {
+      console.log(`Library management API is running on port: ${config.port}`);
+    });
+  } catch (error) {
+    console.error(`Library management API got error: ${error}`);
+  }
+}
+
+server();
