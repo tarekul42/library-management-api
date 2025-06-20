4c5243eb684f5bed1c1d530c77b62754ae3a1416
tarekul42 <tarekulrifat205@gmail.com>
2025-06-20 16:47:53 +0600

    borrow model added

---
+++ src/modules/borrow/borrow.model.ts
@@ -0,0 +1,18 @@
+import { model, Schema } from "mongoose";
+import { IBorrow } from "./borrow.interface";
+
+const borrowSchema = new Schema<IBorrow>(
+  {
+    book: { type: Schema.Types.ObjectId },
+    quantity: { type: Number, min: 1, required: true },
+    dueDate: { type: Date, required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+const Borrow = model("Borrow", borrowSchema);
+
+export default Borrow;
