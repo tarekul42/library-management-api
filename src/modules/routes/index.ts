8ded711ad98875a711f3989d4e887cece824c7df
tarekul42 <tarekulrifat205@gmail.com>
2025-06-20 21:25:15 +0600

    removed user and relavant things

--- src/modules/routes/index.ts
+++ src/modules/routes/index.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import bookRoute from "../book/book.route";
 import borrowRoute from "../borrow/borrow.route";
-import userRoute from "../user/user.route";
 
 const routes = Router();
 
 routes.use("/api/books", bookRoute);
-routes.use("/api/users", userRoute);
 routes.use("/api/borrow", borrowRoute);
 
 export default routes;
--- src/modules/user/user.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { userController } from "./user.controller";
-
-const userRoute = Router();
-
-userRoute.post("/", userController.registerUser);
-userRoute.get("/", userController.getUsers);
-
-export default userRoute;
