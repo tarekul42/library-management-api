96ca8b907eb6b953e5f3cce7bbd0572ffb01b264
tarekul42 <tarekulrifat205@gmail.com>
2025-06-21 14:24:10 +0600

    updated for deployment

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,11 +1,9 @@
-import { defineConfig } from "eslint/config";
-
-export default defineConfig([
-	{
-		files: ["**/*.js", "**/*.cjs", "**/*.mjs"],
-		rules: {
-			"prefer-const": "warn",
-			"no-constant-binary-expression": "error",
-		},
-	},
-]);
+module.exports = [
+    {
+        files: ["**/*.js", "**/*.cjs", "**/*.mjs"],
+        rules: {
+            "prefer-const": "warn",
+            "no-constant-binary-expression": "error",
+        },
+    },
+];
