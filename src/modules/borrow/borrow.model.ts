3cf85e99e114e7450b98a987cdbb9e8174fb4c77
tarekul42 <tarekulrifat205@gmail.com>
2025-06-22 19:11:29 +0600

    added mongoose middleware pre and post

--- src/modules/borrow/borrow.model.ts
+++ src/modules/borrow/borrow.model.ts
@@ -13,7 +13,17 @@ const borrowSchema = new Schema<IBorrow>(
   }
 );
 
+// pre-save middlwware: runs before saving a borrow record
+borrowSchema.pre("save", function (next) {
+  console.log(`About to save borrow record for book ID: ${this.book}`);
+  next();
+});
+
+// post-save middleware: runs after saving a borrow record
+borrowSchema.post("save", function (doc) {
+  console.log(`Borrow record saved successfully for book ID: ${doc._id}`);
+});
+
 const Borrow = model<IBorrow>("Borrow", borrowSchema);
 
 export default Borrow;
-
